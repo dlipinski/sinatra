ead4e06420580a06f1b1f31aabf9b20b98b7f49b
dlipinski <dawid.lipinski3@gmail.com>
2019-02-15 02:10:38 +0100

    Web guesser game complete

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/' do
-    number = rand(100)
-    erb :index, :locals => {:number => number}
+
+set :number => rand(4)
+
+get '/' do    
+    guess = params['guess'].to_i
+    message = check_guess(guess)
+    erb :index, :locals => {:message => message}
+end
+
+def check_guess(guess)
+    if guess > settings.number
+        if guess - settings.number > 5
+            return "Way too high!"
+        else
+            return "Too high!"
+        end
+    elsif guess < settings.number
+        if settings.number - guess > 5
+            return "Way too low!"
+        else
+            return"Too low!"
+        end
+    else
+        return "Correct! The number is " + settings.number.to_s 
+    end
 end
