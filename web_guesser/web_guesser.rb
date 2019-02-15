ebe4c89aea82bf70090356b8e12d45e3c4037947
dlipinski <dawid.lipinski3@gmail.com>
2019-02-15 02:20:20 +0100

    Added coloring background based on guess correctness

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -6,24 +6,24 @@ set :number => rand(4)
 
 get '/' do    
     guess = params['guess'].to_i
-    message = check_guess(guess)
-    erb :index, :locals => {:message => message}
+    locals = check_guess(guess)
+    erb :index, :locals => locals
 end
 
 def check_guess(guess)
     if guess > settings.number
         if guess - settings.number > 5
-            return "Way too high!"
+            return {:message => "Way too high!", :background => "red"}
         else
-            return "Too high!"
+            return {:message => "Too high!", :background => "lightcoral"}
         end
     elsif guess < settings.number
         if settings.number - guess > 5
-            return "Way too low!"
+            return {:message => "Way too low!", :background => "red"}
         else
-            return"Too low!"
+            return {:message => "Too low!", :background => "lightcoral"}
         end
     else
-        return "Correct! The number is " + settings.number.to_s 
+        return {:message => "Correct! The number is " + settings.number.to_s, :background => "green"}
     end
 end
