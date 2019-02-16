230b3572272103d36c3a1143dfd1ccd54d0fe86e
dlipinski <dawid.lipinski3@gmail.com>
2019-02-16 17:28:18 +0100

    Moved slf methods to store and refractored

--- idea_box/idea.rb
+++ idea_box/idea.rb
@@ -1,49 +1,9 @@
 class Idea
-    
-
     attr_reader :title, :description
-
-    def self.all
-        raw_ideas.map do |data|
-            Idea.new(data)
-        end
-    end
-
-    def self.raw_ideas
-        database.transaction do |db|
-            db['ideas'] || []
-        end
-    end
-
-    def self.delete(position)
-        database.transaction do
-            database['ideas'].delete_at(position)
-        end
-    end
-
-    def self.find(id)
-        Idea.new(find_raw_idea(id))
-    end
-
-    def self.find_raw_idea(id)
-        database.transaction do
-            database['ideas'].at(id)
-        end
-    end
-
-    def self.update(id, data)
-        database.transaction do
-            database['ideas'][id] = data
-        end
-    end
 
     def initialize(attributes)
         @title = attributes["title"]
         @description = attributes["description"]
-    end
-
-    def self.database
-        @database ||= YAML::Store.new 'ideabox'
     end
 
     def save
